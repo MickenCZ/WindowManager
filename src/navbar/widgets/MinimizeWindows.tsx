6f6801cc92e730e665b911eb5229f10294ea5039
MickenCZ <MickenCZ@seznam.cz>
2024-08-13 17:45:05 +0200

    minimize all windows button works

--- src/navbar/widgets/MinimizeWindows.tsx
+++ src/navbar/widgets/MinimizeWindows.tsx
@@ -1,8 +1,21 @@
+import { useContext } from "react"
 import "./MinimizeWindows.css"
+import { SetApplicationsContext } from "../../App"
 
 function MinimizeWindows() {
+    const setApplications = useContext(SetApplicationsContext) //for changing state of App.tsx
+    
+    const handleClick = () => setApplications(prevState => {
+        const newState = [...prevState] //change, not mutate state
+        for (const application of newState) {
+            application.active = false
+            application.minimized = true
+        }
+        return newState
+    })
+
     return (
-        <div id="minimizeWindows">
+        <div id="minimizeWindows" onClick={handleClick}>
             <div id="minimizeWindowsButton" tabIndex={0}></div>
         </div>
     )
